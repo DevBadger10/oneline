7705bd622c0d9daf24fe42d69f2377c7664ac36a
GiggleGuide <104037142+DevBadger10@users.noreply.github.com>
2023-02-05 16:16:01 +1100

    Commit

--- Interpreter/Interpreter.py
+++ Interpreter/Interpreter.py
@@ -1,10 +1,21 @@
 import sys
 
+a = 0
+b = 0
+c = 0
+d = 0
+e = 0
+f = 0
+z = 0
+x = 0
+m = 0
+temp = 0
+
 try:
     if sys.argv[1].endswith(".ol"):
         path = sys.argv[1]
     else:
-        print("Interpreter Error: Incorrect Extension.")
+        print("Interpreter Error: Incorrect extension.")
         sys.exit()
 except:
     print("Interpreter Error: No code path supplied.")
@@ -13,6 +24,9 @@ except:
 def init():
     codefile = open(path)
     code = codefile.read()
-    print(code)
+    codefile.close()
+    global line
+    line = code.split(":")
 
 init()
+print(line)
